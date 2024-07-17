c9966bebfbbc51899a14a87ab558791fc853671a
Rafael Pereira Gonçalves <rafael.math14@gmail.com>
2024-07-16 21:29:49 -0300

    Criação do projeto spring com as dependências necessárias.

---
+++ src/main/java/br/com/alura/challange_forum_hub/ChallangeForumHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.challange_forum_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeForumHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeForumHubApplication.class, args);
+	}
+
+}
